daae44a9a8a278a09173b25ab3d01ea890615f32
Nicolas <contact@nicolabricot.com>
2012-05-23 22:09:24 +0200

    Classe de test.

---
+++ src/tests/TestConsole.java
@@ -0,0 +1,19 @@
+package tests;
+
+import view.Console;
+
+public class TestConsole
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Console c = new Console();
+		c.play();
+
+	}
+
+}
